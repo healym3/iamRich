61e1a422db2ae89719ee48baa432a213919d54a1
MacKenzie Healy <healym3@gator.uhd.edu>
2021-10-30 11:45:55 -0500

    upload 2

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by user208358 on 10/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
